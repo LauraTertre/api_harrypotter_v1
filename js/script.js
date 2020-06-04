1fbf88e39d64d46e8af89ae96fca55390e1e5feb
Destineei <59798091+Destineei@users.noreply.github.com>
2020-06-04 19:22:09 +0200

    First commit
    Show informations : Name, Role, House & Species according to : Role/House/Species type

--- js/script.js
+++ js/script.js
@@ -1,23 +1,64 @@
-var infoElt = document.getElementById("information");
+/**
+ * 
+ */
 
-var api_key = "?key=$2a$10$nfs0VuJRTF6UzA7jrPtpvOJt7CSAH6OB975kD24SpBlOioh/WHv/y";
+var formElt = document.querySelector("form");
+var infoElt = document.getElementById("information")
 
+/**
+ * Check what is the value in the form
+ * @param formElt form in the html
+ * @param nameForSearch check what value is check in form
+ * @return {string} search = URI for function AjaxGet
+ */
 
-ajaxGet("https://www.potterapi.com/v1/characters/" +api_key, function (reponse) {
-    // Transformation de la réponse en un objet JSON
-    var character = JSON.parse(reponse);
-    character.forEach(function (character) {
-        var nomElt = document.createElement("h2");
-        nomElt.textContent = character.name;
-        var roleElt = document.createElement("div");
-        roleElt.textContent = character.role;
-        var houseElt = document.createElement("div");
-        houseElt.textContent = character.house;
-        
+formElt.addEventListener("submit", function(e) {
+    e.preventDefault();
+    if (document.getElementById('role').selected==true) {
+        var nameForSearch = formElt.elements.search.value;
+        search = "&role="+nameForSearch;
+    }
+    else if (document.getElementById('house').selected==true) {
+        var nameForSearch = formElt.elements.search.value;
+        search = "&house="+nameForSearch;
+    }
+    else if (document.getElementById('species').selected==true) {
+        var nameForSearch = formElt.elements.search.value;
+        search = "&species="+nameForSearch;
+    }
+    else {
+        console.log("Retente autre chose...");
+    }
+
+/**
+ * Request and create information in html
+ * @param {url} HarryPotterPublicApi 
+ * @param {string} api_key for using public api
+ * @param {string} search using for request
+ * @return {elements for dom} Informations to show in HTML
+ */
+
+    var api_key = "?key=$2a$10$nfs0VuJRTF6UzA7jrPtpvOJt7CSAH6OB975kD24SpBlOioh/WHv/y";
+
+    ajaxGet("https://www.potterapi.com/v1/characters/" + api_key + search, function (reponse) {
+        // Transformation de la réponse en un objet JSON
+        var character = JSON.parse(reponse);
+        character.forEach(function (character) {
+            var nomElt = document.createElement("h2");
+            nomElt.textContent = character.name;
+            var roleElt = document.createElement("div");
+            roleElt.textContent = character.role;
+            var houseElt = document.createElement("div");
+            houseElt.textContent = character.house;
+            var speciesElt = document.createElement("div");
+            speciesElt.textContent = character.species;
+
             //Affichage des informations
             infoElt.appendChild(nomElt);
             infoElt.appendChild(roleElt);
             infoElt.appendChild(houseElt);
-
-    })
+            infoElt.appendChild(speciesElt);
+    
+        })
+    });
 });
